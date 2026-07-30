99079e2be6816d80658756a729a992164fb6ac99
Daniel Gran, B. Eng. <contact@grandaniel.com>
2026-07-30 12:01:44 +0000

    fixed close on image context menu

--- src/components/MarkdownEditor/ContextMenu/MarkdownEditorImageContextMenu.vue
+++ src/components/MarkdownEditor/ContextMenu/MarkdownEditorImageContextMenu.vue
@@ -1,6 +1,7 @@
 <template>
   <MarkdownEditorContextMenu
     :x="x"
+    ref="contextMenuRef"
     :y="y"
   >
     <div class="markdown-editor-image-context-menu-content">
@@ -12,8 +13,12 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import MarkdownEditorContextMenu from "./MarkdownEditorContextMenu.vue";
 import MarkdownEditorContextMenuBlockItem from "./MarkdownEditorContextMenuBlockItem.vue";
+import { onClickOutside } from "@vueuse/core";
+
+const contextMenuRef = ref<InstanceType<typeof MarkdownEditorContextMenu> | null>(null);
 
 defineProps<{
   x: number;
@@ -22,7 +27,12 @@ defineProps<{
 
 const emit = defineEmits<{
   editAttributes: [];
+  close: [];
 }>();
+
+onClickOutside(contextMenuRef, () => {
+  emit('close');
+});
 </script>
 
 <style lang="scss" scoped>
